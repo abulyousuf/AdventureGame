d043e65df8e70debcdb9428b4d473d95ab457c31
abulyousuf <abulyousuf@outlook.com>
2025-08-11 23:00:59 +0100

    Create variables

--- AdventureGame.js
+++ AdventureGame.js
@@ -25,3 +25,18 @@ console.log("\nWelcome, " + playerName + "! Your adventure begins now.");
 console.log(
   "You start with " + playerHealth + " health and " + playerGold + " gold."
 );
+
+// Weapon damage (starts at 0 until player buys a sword)
+let weaponDamage = 0; // Will increase to 10 when player gets a sword
+console.log("Starting weapon damage: " + weaponDamage);
+console.log("When you buy a sword, your weapon damage will increase to 10!");
+
+// Monster defense (affects combat outcomes)
+let monsterDefense = 5; // Monster's defense value
+console.log("Monster defense: " + monsterDefense);
+console.log("Monsters can withstand some damage in combat!");
+
+// Healing potion restoration (matches final implementation)
+let healingPotionValue = 30; // How much health is restored
+console.log("Healing potion value: " + healingPotionValue);
+console.log("A potion will restore " + healingPotionValue + " health!");
